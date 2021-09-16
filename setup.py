5c7f8a0c546ded586ba770606763606957bb9c5b
David <32926550+davidusb-geek@users.noreply.github.com>
2021-09-16 13:48:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ setup(
         'netcdf4>=1.5.3',
         'tables>=3.6.1',
         'sphinx==3.5.4',
-        'sphinx-rtd-theme==0.5.2',
         'myst-parser==0.14.0'
     ],  # Optional
     entry_points={  # Optional
