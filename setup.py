efba78e4ee46d9d289df023b0e06429dc5fd2766
David HERNANDEZ <david.hernandez@NTAI.priv>
2021-10-14 17:00:13 +0200

    Fix - Reverted back to old setup.py file after uncompatibility problem

--- setup.py
+++ setup.py
@@ -6,69 +6,26 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages, Command
-import pathlib, os, sys
-
-NAME = 'emhass', # Required
-VERSION = '0.2.0',  # Required
-DESCRIPTION = 'An Energy Management System for Home Assistant', # Optional
-URL = 'https://github.com/davidusb-geek/emhass',  # Optional
-AUTHOR = 'David HERNANDEZ',  # Optional
-EMAIL = 'davidusb@gmail.com',  # Optional
+from setuptools import setup, find_packages
+import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(VERSION))
-        os.system('git push --tags')
-
-        sys.exit()
-
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    name=NAME
-    version=VERSION
-    description=DESCRIPTION
-    url=URL
-    author=AUTHOR
-    author_email=EMAIL
+    name='emhass',  # Required
+    version='0.2.0',  # Required
+    description='An Energy Management System for Home Assistant',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/davidusb-geek/emhass',  # Optional
+    author='David HERNANDEZ',  # Optional
+    author_email='davidusb@gmail.com',  # Optional
     classifiers=[  # Optional
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -88,8 +45,8 @@ setup(
     install_requires=[
         'numpy>=1.20.1',
         'pandas>=1.2.3',
-        'pvlib>=0.9.0',
-        'protobuf>=3.0.0',
+        'pvlib>=0.8.1',
+        'protobuf>=3.0.0a3',
         'siphon>=0.9',
         'pytz>=2021.1',
         'requests>=2.25.1',
