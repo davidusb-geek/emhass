0de77aa5119120f79911b051f63f4f944b5ba919
davidusb-geek <davidusb@gmail.com>
2023-05-27 16:40:46 +0200

    Fix - Fixed wrong path for csv files when using load cost and prod price forecasts

--- tests/test_forecast.py
+++ tests/test_forecast.py
@@ -413,7 +413,8 @@ class TestForecast(unittest.TestCase):
         df_input_data = self.fcst.get_load_cost_forecast(self.df_input_data)
         self.assertTrue(self.fcst.var_load_cost in df_input_data.columns)
         self.assertTrue(df_input_data.isnull().sum().sum()==0)
-        df_input_data = self.fcst.get_load_cost_forecast(self.df_input_data, method='csv')
+        df_input_data = self.fcst.get_load_cost_forecast(self.df_input_data, method='csv',
+                                                         csv_path='/data/data_load_cost_forecast.csv')
         self.assertTrue(self.fcst.var_load_cost in df_input_data.columns)
         self.assertTrue(df_input_data.isnull().sum().sum()==0)
         
@@ -421,7 +422,8 @@ class TestForecast(unittest.TestCase):
         df_input_data = self.fcst.get_prod_price_forecast(self.df_input_data)
         self.assertTrue(self.fcst.var_prod_price in df_input_data.columns)
         self.assertTrue(df_input_data.isnull().sum().sum()==0)
-        df_input_data = self.fcst.get_prod_price_forecast(self.df_input_data, method='csv')
+        df_input_data = self.fcst.get_prod_price_forecast(self.df_input_data, method='csv',
+                                                          csv_path='/data/data_load_cost_forecast.csv')
         self.assertTrue(self.fcst.var_prod_price in df_input_data.columns)
         self.assertTrue(df_input_data.isnull().sum().sum()==0)
         
