c8b71e11ce0aef8ee3315fc758285cfed7de3c55
Giel Janssens <gieljnssns@me.com>
2024-01-07 08:24:21 +0100

    Add csv-prediction

--- src/emhass/web_server.py
+++ src/emhass/web_server.py
@@ -13,6 +13,7 @@ from distutils.util import strtobool
 from emhass.command_line import set_input_data_dict
 from emhass.command_line import perfect_forecast_optim, dayahead_forecast_optim, naive_mpc_optim
 from emhass.command_line import forecast_model_fit, forecast_model_predict, forecast_model_tune
+from emhass.command_line import csv_predict
 from emhass.command_line import publish_data
 from emhass.utils import get_injection_dict, get_injection_dict_forecast_model_fit, \
     get_injection_dict_forecast_model_tune, build_params
@@ -189,6 +190,11 @@ def action_call(action_name):
         if not checkFileLog(ActionStr):
             return make_response(msg, 201)
         return make_response(grabLog(ActionStr), 400)
+    elif action_name == 'csv-predict':
+        app.logger.info(" >> Performing a csv predict...")
+        csv_predict(input_data_dict, app.logger)
+        msg = f'EMHASS >> Action csv-predict executed... \n'
+        return make_response(msg, 201)
     else:
         app.logger.error("ERROR: passed action is not valid")
         msg = f'EMHASS >> ERROR: Passed action is not valid... \n'
