0e2f535a31e51b718724adce548dda48e340e9a4
Giel Janssens <gieljnssns@me.com>
2024-01-09 21:11:13 +0100

    cleanup

--- src/emhass/csv_predictor.py
+++ src/emhass/csv_predictor.py
@@ -31,11 +31,13 @@ class CsvPredictor:
     
     It exposes one main method:
     
-    - `predict`: to obtain a forecast from a pre-trained model.
+    - `predict`: to obtain a forecast from a csv file.
     
     """
 
-    def __init__(self, data: pd.DataFrame, model_type: str, csv_file: str, independent_variables: list, dependent_variable: str, sklearn_model: str, new_values:list, root: str,
+    # def __init__(self, data: pd.DataFrame, model_type: str, csv_file: str, independent_variables: list, dependent_variable: str, sklearn_model: str, new_values:list, root: str,
+    #               logger: logging.Logger) -> None:
+    def __init__(self, csv_file: str, independent_variables: list, dependent_variable: str, sklearn_model: str, new_values:list, root: str,
                   logger: logging.Logger) -> None:
         r"""Define constructor for the forecast class.
 
@@ -44,23 +46,28 @@ class CsvPredictor:
         :param model_type: A unique name defining this model and useful to identify \
             for what it will be used for.
         :type model_type: str
-        :param var_model: The name of the sensor to retrieve data from Home Assistant. \
-            Example: `sensor.power_load_no_var_loads`.
-        :type var_model: str
+        :param csv_file: The name of the csv file to retrieve data from. \
+            Example: `prediction.csv`.
+        :type csv_file: str
+        :param independent_variables: A list of independent variables. \
+            Example: [`solar`, `degree_days`].
+        :type independent_variables: list
+        :param dependent_variable: The dependent variable(to be predicted). \
+            Example: `hours`.
+        :type dependent_variable: str
         :param sklearn_model: The `scikit-learn` model that will be used. For now only \
             this options are possible: `LinearRegression`, `ElasticNet` and `KNeighborsRegressor`.
         :type sklearn_model: str
-        :param num_lags: The number of auto-regression lags to consider. A good starting point \
-            is to fix this as one day. For example if your time step is 30 minutes, then fix this \
-            to 48, if the time step is 1 hour the fix this to 24 and so on.
-        :type num_lags: int
+        :param new_values: The new values for the independent variables(in the same order as the independent variables list). \
+            Example: [2.24, 5.68].
+        :type new_values: list
         :param root: The parent folder of the path where the config.yaml file is located
         :type root: str
         :param logger: The passed logger object
         :type logger: logging.Logger
         """
-        self.data = data
-        self.model_type = model_type
+        # self.data = data
+        # self.model_type = model_type
         self.csv_file = csv_file
         self.independent_variables = independent_variables
         self.dependent_variable = dependent_variable
@@ -86,18 +93,30 @@ class CsvPredictor:
             raise ValueError(
                 f"CSV file should contain the following columns: {', '.join(required_columns)}"
             )
+        print(type(data))
         return data
     
     def prepare_data(self, data):
+        """
+        Prepare the data.
+        
+        :param data: Input Data
+        :return: Input DataFrame with freq defined
+        :rtype: pd.DataFrame
+        
+        """
         X = data[self.independent_variables].values
         y = data[self.dependent_variable].values
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        print(type(X_train))
+        print(type(y_train))
         
         return X_train, y_train
     
     
-    def predict(self, perform_backtest: Optional[bool] = False
-            ) -> pd.Series:
+    # def predict(self, perform_backtest: Optional[bool] = False
+    #         ) -> pd.Series:
+    def predict(self):
         r"""The fit method to train the ML model.
 
         :param split_date_delta: The delta from now to `split_date_delta` that will be used \
@@ -109,7 +128,7 @@ class CsvPredictor:
         :return: The DataFrame containing the forecast data results without and with backtest
         :rtype: Tuple[pd.DataFrame, pd.DataFrame]
         """
-        self.logger.info("Performing a forecast model fit for "+self.model_type)
+        self.logger.info("Performing a prediction for "+self.csv_file)
         # Preparing the data: adding exogenous features
         data = self.load_data()
         X, y = self.prepare_data(data)
@@ -131,6 +150,7 @@ class CsvPredictor:
         self.logger.info(f"Elapsed time for model fit: {time.time() - start_time}")
         new_values = np.array([self.new_values])
         prediction = self.forecaster.predict(new_values)
+        print(type(prediction))
         
         return prediction
     
