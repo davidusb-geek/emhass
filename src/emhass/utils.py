73f0aefdd39886229f4f704ddb070f892adc43b1
Michaël Piron <4469877+michaelpiron@users.noreply.github.com>
2024-01-24 20:01:05 +0100

    Update utils.py

--- src/emhass/utils.py
+++ src/emhass/utils.py
@@ -420,11 +420,11 @@ def get_yaml_parse(config_path: str, use_secrets: Optional[bool] = True,
     retrieve_hass_conf['freq'] = pd.to_timedelta(retrieve_hass_conf['freq'], "minutes")
     retrieve_hass_conf['time_zone'] = pytz.timezone(retrieve_hass_conf['time_zone'])
     
-    optim_conf = dict((key,d[key]) for d in input_conf['optim_conf'] for key in d)
+    optim_conf = input_conf.get('optim_conf', {})
     optim_conf['list_hp_periods'] = dict((key,d[key]) for d in optim_conf['list_hp_periods'] for key in d)
     optim_conf['delta_forecast'] = pd.Timedelta(days=optim_conf['delta_forecast'])
     
-    plant_conf = dict((key,d[key]) for d in input_conf['plant_conf'] for key in d)
+    plant_conf = input_conf.get('plant_conf', {})
     
     return retrieve_hass_conf, optim_conf, plant_conf
 
