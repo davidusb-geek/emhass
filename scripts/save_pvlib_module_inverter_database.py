803a13844f4690f840e8c016069c04d83a3a5d0e
GeoDerp <18461782+GeoDerp@users.noreply.github.com>
2025-01-04 14:00:58 +0000

    CEC append custom EMHASS modules and inverters

--- scripts/save_pvlib_module_inverter_database.py
+++ scripts/save_pvlib_module_inverter_database.py
@@ -20,7 +20,7 @@ from emhass.utils import get_logger, get_root
 # the root folder
 root = pathlib.Path(str(get_root(__file__, num_parent=2)))
 emhass_conf = {}
-emhass_conf["data_path"] = root / "data/"
+emhass_conf["data_path"] = root / "src/emhass/data/"  # NOTE CUSTOM DATA PATH
 emhass_conf["root_path"] = root / "src/emhass/"
 emhass_conf["docs_path"] = root / "docs/"
 emhass_conf["config_path"] = root / "config.json"
@@ -58,8 +58,11 @@ if __name__ == "__main__":
     cec_inverters_emhass = pvlib.pvsystem.retrieve_sam(
         path=str(emhass_conf["data_path"] / "emhass_inverters.csv")
     )
+    logger.info("=================")
+    logger.info("=================")
 
     logger.info("Updating and saving databases")
+
     # Modules
     cols_to_keep_modules = [
         elem
@@ -68,7 +71,7 @@ if __name__ == "__main__":
     ]
     cec_modules = pd.concat([cec_modules, cec_modules_0[cols_to_keep_modules]], axis=1)
     logger.info(
-        f"Number of elements from old database copied in new database for modules = {len(cols_to_keep_modules)}"
+        f"Number of elements from old database copied in new database for modules = {len(cols_to_keep_modules)}:"
     )
     cols_to_keep_modules = [
         elem
@@ -79,8 +82,18 @@ if __name__ == "__main__":
         [cec_modules, cec_modules_emhass[cols_to_keep_modules]], axis=1
     )
     logger.info(
-        f"Number of elements from custom EMHASS database copied in new database for modules = {len(cols_to_keep_modules)}"
+        f"Number of elements from custom EMHASS database copied in new database for modules = {len(cols_to_keep_modules)}:"
     )
+    print(
+        tabulate(
+            cec_modules_emhass[cols_to_keep_modules].head(20).iloc[:, :5],
+            headers="keys",
+            tablefmt="psql",
+        )
+    )
+    logger.info("=================")
+    logger.info("=================")
+
     # Inverters
     cols_to_keep_inverters = [
         elem
@@ -104,15 +117,22 @@ if __name__ == "__main__":
     logger.info(
         f"Number of elements from custom EMHASS database copied in new database for inverters = {len(cols_to_keep_inverters)}"
     )
+    print(
+        tabulate(
+            cec_inverters_emhass[cols_to_keep_inverters].head(20).iloc[:, :5],
+            headers="keys",
+            tablefmt="psql",
+        )
+    )
+    logger.info("=================")
+    logger.info("=================")
     logger.info("Modules databases")
     print(tabulate(cec_modules.head(20).iloc[:, :5], headers="keys", tablefmt="psql"))
     logger.info("Inverters databases")
     print(tabulate(cec_inverters.head(20).iloc[:, :3], headers="keys", tablefmt="psql"))
     if save_new_files:
-        with bz2.BZ2File(emhass_conf["root_path"] + "/data/cec_modules.pbz2", "w") as f:
+        with bz2.BZ2File(emhass_conf["data_path"] / "cec_modules.pbz2", "w") as f:
             cPickle.dump(cec_modules, f)
     if save_new_files:
-        with bz2.BZ2File(
-            emhass_conf["root_path"] + "/data/cec_inverters.pbz2", "w"
-        ) as f:
+        with bz2.BZ2File(emhass_conf["data_path"] / "cec_inverters.pbz2", "w") as f:
             cPickle.dump(cec_inverters, f)
