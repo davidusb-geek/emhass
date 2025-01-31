c477322f13fd6c340971cb357f2e07697d5fca20
GeoDerp <18461782+GeoDerp@users.noreply.github.com>
2025-01-31 11:43:57 +0000

    fix(packadge): rm strtobool

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,10 @@
 import os
-from distutils.util import strtobool
 
 bind = f"{os.getenv('IP', '0.0.0.0')}:{os.getenv('PORT', '5000')}"
 
 workers = int(os.getenv("WEB_CONCURRENCY", 1))
 threads = int(os.getenv("PYTHON_MAX_THREADS", 8))
 
-reload = bool(strtobool(os.getenv("WEB_RELOAD", "true")))
+reload = (os.getenv("WEB_RELOAD", "true").lower()) == "true"
 
 timeout = int(os.getenv("WEB_TIMEOUT", 240))
